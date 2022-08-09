57f8f83d430c232aa0b412e2ae4eba92cd8b5091
elizabeseye <90814043+elizabeseye@users.noreply.github.com>
2022-08-09 18:40:36 +0800

    Add files via upload

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,20 +1,20 @@
-const axios = require("axios");
-const API_URL = "https://jsonplaceholder.typicode.com/todos";
+const axios = require('axios');
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
 
 export const getTodoAPI = async (data) => {
-  try {
-    const response = await axios.get(`${API_URL}/${data}`);
-    return response.data;
-  } catch (err) {
-    throw new Error(err);
-  }
+	try {
+		const response = await axios.get(`${API_URL}/${data.id}`);
+		return response.data;
+	} catch (err) {
+		throw new Error(err);
+	}
 };
 
 export const addTodoAPI = async (data) => {
-  try {
-    const response = await axios.post(API_URL, data);
-    return response.data;
-  } catch (err) {
-    throw new Error(err);
-  }
+	try {
+		const response = await axios.post(API_URL, data);
+		return response.data;
+	} catch (err) {
+		throw new Error(err);
+	}
 };
